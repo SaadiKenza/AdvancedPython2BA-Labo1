85ca4d398591559eba0e94f9829c67d99322422e
Saadi <23268@ecam.be>
2025-04-19 16:34:10 +0200

    add test for the roots function

--- test_utils.py
+++ test_utils.py
@@ -6,9 +6,9 @@ def test_fact():
     assert utils.fact(5)==120
     assert utils.fact(-5)==ValueError
 
-# def test_roots():
-#     # À compléter...
-#     pass
+def test_roots():
+    assert utils.roots(1,0,1)==(1j,-1j)
+
 
 # def test_integrate():
 #     # À compléter...
